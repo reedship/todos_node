1d97b938af43176e3d4796a78c2deb571f71670d
Reed Shipley <reedbshipley@gmail.com>
2024-04-14 12:22:58 -0500

    working commit

--- main.js
+++ main.js
@@ -6,17 +6,26 @@ const { ExtensionMap } = require('./extensions.js');
 const CommentRegex = /^[ |\t]*\/\/.*(?:todo|fixme|note|fix me)/i
 const ENV_DIRECTORY = process.env.TODOS_DIRECTORY || undefined;
 const ENV_OUTPUT = process.env.TODOS_OUTPUT || undefined;
+const GITHUB_URL = process.env.TODOS_GITHUB || undefined; // should have format of 'OWNER/REPO'
+const GITHUB_ACCESS_TOKEN = process.env.TODOS_TOKEN || undefined;
 
 function printHelp() {
-    const help =
-	  "Todos \n\t v.0.0.1 \n\t author: Reed Shipley \n" +
-	  "Usage:\n\t" +
-	  "todos --dir=<directory> --output=<output> \n\t";
+    const help = [
+	"Todos \nv.0.0.1",
+	"This cli scans a directory of your choosing and lets you print an output",
+	"Variables:",
+	"dir: the directory you'd like to scan. Defaults to the current directory.",
+	"output: output format. Defaults to STDOUT. Options are CSV or STDOUT. If CSV is selected, the output will be written to `output.csv` in the current working directory",
+	"Usage:",
+	"$ todos --dir=<directory> --output=<output>"
+    ].join("\n");
     console.log(help);
+    return 1;
 }
 
 function getArgs() {
     const argsList = process.argv
+    if (argsList.includes("help")) printHelp();
     const parsedArgs = {
 	dir: path.resolve(__dirname),
 	output: 'STDOUT'
@@ -125,6 +134,54 @@ function writeOutput(args, todos) {
     }
 }
 
+async function getIssues() {
+    const octokit = new OctoKit({auth: GITHUB_ACCESS_TOKEN});
+    const issues = await octokit.request(`GET /repos/${GITHUB_URL}/issues`, {
+	owner: GITHUB_URL.split('/')[0],
+	repo: GITHUB_URL.split('/')[1],
+	headers: {
+	    'X-GitHub-Api_Version': '2022-11-28'
+	}
+    });
+    console.log({issues});
+    return issues;
+}
+
+function filterIssues(todos, issues) {
+    // we need to create a specific format for the naming convention so that we can check for issues that match that pattern.
+    // TODO:{FileName}:{LineNumber} does the below regex match?
+    const patt = new RegExp("/TODO:([a-z][A-Z])*:([0-9])*/");
+    const todosIssues = issues.select((issue) => patt.test(issue.title));
+    const issuesToBeRemoved = [];
+    todosIssues.forEach((issue)=> {
+	if (!todos.values().contains(issue.title)) {
+	    issuesToBeRemoved.push(issue);
+	}
+    });
+    console.log(`Found ${issuesToBeremoved.length} issues that can be removed from github`);
+    return issuesToBeRemoved;
+}
+
+async function deleteGithubIssues(issuesToBeRemoved) {
+    // IMPLEMENT THIS
+    issuesToBeRemoved.forEach((issue) => {
+	console.log(`Deleting ${issue.title}`);
+    });
+}
+
+//TODO: TEST THIS FUNCTION
+async function handleIssues(todos) {
+    // check if we have the envr variables set
+    if (GITHUB_URL == undefined || GITHUB_ACCESS_TOKEN == undefined) {
+	console.log("Github action not configured. Exiting");
+	return;
+    }
+    const issues = await getIssues();
+    issuesToBeRemoved = filterIssues(todos, issues);
+    await deleteGithubIssues(issuesToBeRemoved);
+}
+
+// the below comment is for testing purposes.
 // TODO: Finish this function
 async function main() {
     if (process.argv.length === 3 && process.argv[2].includes('help')){
@@ -142,5 +199,8 @@ async function main() {
     }
     todos = todos.flat();
     writeOutput(args,todos);
+    handleIssues(todos);
 }
+
+
 main()

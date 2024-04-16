c531ce1655acea5ba2dbb8a02d85f2702fb11024
Reed Shipley <reedbshipley@gmail.com>
2024-04-15 22:08:13 -0500

    Basic github octokit integration
    Need to work on issue creation that is not tied to line number. :P

---
+++ cleanupIssues.js
@@ -0,0 +1,29 @@
+const { Octokit, App } = require("octokit");
+const GITHUB_URL = process.env.TODOS_GITHUB || undefined;
+const GITHUB_ACCESS_TOKEN = process.env.TODOS_TOKEN || undefined;
+const octokit = new Octokit({auth: GITHUB_ACCESS_TOKEN});
+
+const [owner, repo] = GITHUB_URL.split('/');
+async function main() {
+    const issues = await octokit.request(`GET /repos/${GITHUB_URL}/issues`, {
+	owner: owner,
+	repo: repo,
+	headers: {
+	    'X-GitHub-Api_Version': '2022-11-28'
+	}
+    });
+    let openIssues = issues.data;
+    if (!openIssues) {
+	console.log("NO ISSUES OPEN")
+	return;
+    }
+    console.log({openIssues});
+    for (let i=0; i< openIssues.length; i++) {
+	let result = await octokit.request(`PATCH /repos/${owner}/${repo}/issues/${openIssues[i].number}`, {
+	    state: 'closed',
+	    headers: {'X-GitHub-Api-Version': '2022-11-28'}
+	});
+	console.log({result});
+    }
+}
+main();
--- main.js
+++ main.js
@@ -2,22 +2,36 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 const { ExtensionMap } = require('./extensions.js');
-
+const { Octokit, App } = require("octokit");
 const CommentRegex = /^[ |\t]*\/\/.*(?:todo|fixme|note|fix me)/i
 const ENV_DIRECTORY = process.env.TODOS_DIRECTORY || undefined;
 const ENV_OUTPUT = process.env.TODOS_OUTPUT || undefined;
 const GITHUB_URL = process.env.TODOS_GITHUB || undefined; // should have format of 'OWNER/REPO'
 const GITHUB_ACCESS_TOKEN = process.env.TODOS_TOKEN || undefined;
 
+const octokit = new Octokit({auth: GITHUB_ACCESS_TOKEN});
+
+
 function printHelp() {
     const help = [
 	"Todos \nv.0.0.1",
+	"",
 	"This cli scans a directory of your choosing and lets you print an output",
-	"Variables:",
+	"",
+	"There are two environment variables you must set up in order to connect this cli to your github repo.",
+	"",
+	"Environment Variables:",
+	"GITHUB_URL: the location of your github repo,with 'https://github.com/' removed",
+	"    example: johnsmith/test_repo",
+	"GITHUB_ACCESS_TOKEN: the access token you have created for your github account",
+	"    get value from https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens",
+	"",
+	"Command Line Variables:",
 	"dir: the directory you'd like to scan. Defaults to the current directory.",
 	"output: output format. Defaults to STDOUT. Options are CSV or STDOUT. If CSV is selected, the output will be written to `output.csv` in the current working directory",
+	"",
 	"Usage:",
-	"$ todos --dir=<directory> --output=<output>"
+	"$ GITHUB_URL=todos --dir=<directory> --output=<output>"
     ].join("\n");
     console.log(help);
     return 1;
@@ -62,7 +76,9 @@ async function searchFile(file) {
 	if (line.match(CommentRegex)) {
 	    const fileName = file.split('/').pop();
 	    const fileType = ExtensionMap[fileName.split('.').pop()];
+	    const title = `TODO:${fileName}:${lineNumber}`;
 	    lines.push({
+		title,
 		filePath: file,
 		fileName,
 		fileType,
@@ -75,6 +91,7 @@ async function searchFile(file) {
 }
 
 async function getFiles(directory) {
+    if (directory.includes("node_modules")) return [];
     let unreadFiles = [];
     const items = fs.readdirSync(directory, { withFileTypes: true });
     for (let i=0; i < items.length; ++i){
@@ -95,27 +112,27 @@ async function getFiles(directory) {
 }
 
 function writeOutput(args, todos) {
-    if (args.output.toLowerCase() === 'csv') {
+    // we should sort these todos alphabetically by filename first and then by linenumber
+    todos = todos.sort((a,b) => a.fileName < b.fileName || a.lineNumber < b.lineNumber)
+    if (args.output.toLowerCase().includes('csv')) {
 	// write to generated output.csv file
 	console.log(`Writing ${todos.length} todos to csv...`);
-	// include "completed" checkbox column
-	todos.map(todo => todo.completed = false);
 	const csvString = [
 	    [
+		'Title',
 		'File Path',
 		'File Name',
 		'File Type',
 		'Line Number',
-		'Line',
-		'Completed'
+		'Content'
 	    ],
 	    ...todos.map(todo=>[
+		todo.title,
 		todo.filePath,
 		todo.fileName,
 		todo.fileType,
 		todo.lineNumber,
-		todo.line,
-		todo.completed
+		todo.content
 	    ].map(str => `"${str.toString().replace(/"/g, '\"')}"`))
 	]
 	      .map(e=>e.join(','))
@@ -130,46 +147,45 @@ function writeOutput(args, todos) {
 	});
     } else {
 	// assume STDOUT
-	console.log(todos);
     }
 }
 
 async function getIssues() {
-    const octokit = new OctoKit({auth: GITHUB_ACCESS_TOKEN});
+    const {owner, repo} = GITHUB_URL.split('/');
     const issues = await octokit.request(`GET /repos/${GITHUB_URL}/issues`, {
-	owner: GITHUB_URL.split('/')[0],
-	repo: GITHUB_URL.split('/')[1],
+	owner: owner,
+	repo: repo,
 	headers: {
 	    'X-GitHub-Api_Version': '2022-11-28'
 	}
     });
-    console.log({issues});
-    return issues;
+    let openIssues = issues.data.map(x => x.state === 'open');
+    return openIssues;
 }
 
 function filterIssues(todos, issues) {
-    // we need to create a specific format for the naming convention so that we can check for issues that match that pattern.
-    // TODO:{FileName}:{LineNumber} does the below regex match?
-    const patt = new RegExp("/TODO:([a-z][A-Z])*:([0-9])*/");
-    const todosIssues = issues.select((issue) => patt.test(issue.title));
+    if (!issues.data) return {todos, issuesToBeRemoved: []};
+    const patt = new RegExp("/(?:todo|fixme|note|fix me)\:.*\:\d*/gi");
+    const todosIssues = issues.data.select((issue) => patt.test(issue.title));
     const issuesToBeRemoved = [];
-    todosIssues.forEach((issue)=> {
+    todosIssues.forEach((issue,index)=> {
 	if (!todos.values().contains(issue.title)) {
-	    issuesToBeRemoved.push(issue);
+	    todos.splice(index,1); //remove this item from our todos
+	    issuesToBeRemoved.push(issue); // add to list of items to be
 	}
     });
-    console.log(`Found ${issuesToBeremoved.length} issues that can be removed from github`);
-    return issuesToBeRemoved;
+    console.log(`Found ${issuesToBeremoved.length} issues that can be closed on github`);
+    return [todos, issuesToBeRemoved];
 }
 
 async function deleteGithubIssues(issuesToBeRemoved) {
+    if (!issuesToBeRemoved) return;
     // IMPLEMENT THIS
     issuesToBeRemoved.forEach((issue) => {
 	console.log(`Deleting ${issue.title}`);
     });
 }
 
-//TODO: TEST THIS FUNCTION
 async function handleIssues(todos) {
     // check if we have the envr variables set
     if (GITHUB_URL == undefined || GITHUB_ACCESS_TOKEN == undefined) {
@@ -177,8 +193,28 @@ async function handleIssues(todos) {
 	return;
     }
     const issues = await getIssues();
-    issuesToBeRemoved = filterIssues(todos, issues);
-    await deleteGithubIssues(issuesToBeRemoved);
+    const result = filterIssues(todos, issues);
+    await deleteGithubIssues(result.issuesToBeRemoved);
+    await uploadNewGithubIssues(result.todos);
+}
+
+async function uploadNewGithubIssues(todos) {
+    if (!todos || !GITHUB_URL) return;
+    console.log({GITHUB_URL});
+    const [owner, repo] = GITHUB_URL.split('/');
+    console.log(owner,repo);
+    for (let i=0;i<todos.length;i++){
+	let result = await octokit.request(`POST /repos/${owner}/${repo}/issues`, {
+	    owner: owner,
+	    repo: repo,
+	    title: todos[i].title,
+	    body: todos[i].content,
+	    headers: {'X-GitHub-Api-Version': '2022-11-28'}
+	});
+	if (result) {
+	    console.log({result});
+	}
+    }
 }
 
 // the below comment is for testing purposes.
@@ -202,5 +238,6 @@ async function main() {
     handleIssues(todos);
 }
 
-
-main()
+console.log("Scraping todos..");
+main();
+console.log("Finished scraping todos");
